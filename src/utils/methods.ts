c0f3e9205b7d3c7dbe5ec33df1e82883dcb20556
吴焯斌 <920658022@qq.com>
2023-03-04 14:50:06 +0800

    feat:fix

--- src/utils/methods.ts
+++ src/utils/methods.ts
@@ -42,3 +42,13 @@ export function arrayToTree<T>(ary: T[], root: string | null): T[] {
   }
   return result;
 }
+
+export function getFormId(e = 32) {
+  const t = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+  const a = t.length;
+  let n = '';
+  for (let i = 0; i < e; i++) {
+    n += t.charAt(Math.floor(Math.random() * a));
+  }
+  return `form-id-${n}`;
+}
--- src/views/form/sandbox/index.tsx
+++ src/views/form/sandbox/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useId, useMemo, useState } from 'react';
 import { ReactSortable, ItemInterface } from 'react-sortablejs';
 import _cloneDeep from 'lodash-es/cloneDeep';
-import { arrayToTree } from '@/utils/methods';
+import { arrayToTree, getFormId } from '@/utils/methods';
 
 import './index.less';
 
@@ -21,37 +21,52 @@ const component = [
 ];
 
 const mainKey = 'form-key';
+/**
+ * 组件嵌套
+ * 1、组件itemId随机（form-key-xxx），作为mapKey，确保不重复
+ * 2、用一维数组收集所有表单，用itemId和parentId关联
+ * 3、一维数组转树结构
+ * 4、筛除已存在的表单项
+ */
+
+const formKey = (id, parent = mainKey) => `${parent}-${id}`;
 
 export default function Index() {
   const [components, setComponents] = useState<ItemType[]>(component);
   const [container, setContainer] = useState<ItemType[]>([]);
-  let allSelected = [];
-
-  const formKey = (id, parent = mainKey) => `${parent}-${id}`;
-
+  const [allSelected, setAllSelect] = useState([]);
   const setMainContainer = (selected: ItemType[], isRoot, parent?: ItemType) => {
-    console.log(selected, isRoot, parent);
-    if (parent && !parent.children) return;
-    const tempSelected = _cloneDeep(selected).map((s, i) => {
-      s.itemId = formKey(i + 1, parent?.itemId || mainKey);
-      s.parentId = parent?.itemId || null;
+    console.log(555, selected, isRoot, parent, allSelected);
+    if (!selected.length && !!parent) {
+      return;
+    }
+    const tempSelected: ItemType[] = _cloneDeep(selected).map((s, i) => {
+      s.itemId = s.itemId || getFormId();
+      s.parentId = s.parentId || parent?.itemId || null;
       if (s.compType === 'wrap') {
         s.children = s.children || [];
       }
       return s;
     });
+    let current: ItemType[];
     if (isRoot) {
-      allSelected = tempSelected;
+      current = tempSelected;
     } else {
-      allSelected = [...allSelected, ...tempSelected];
+      // 筛除已存在的表单项
+      const filterSelected =
+        allSelected.length === 0
+          ? tempSelected
+          : tempSelected.filter((t) => allSelected.findIndex((f) => f.itemId === t.itemId) === -1);
+      current = [...allSelected, ...filterSelected];
     }
-    console.log('allSelected', allSelected);
-    const tree = arrayToTree<ItemType>(allSelected, null);
+    setAllSelect(current);
+    console.log('allSelected', current, tempSelected);
+    const tree = arrayToTree<ItemType>(current, null);
     console.log('tree', tree);
     setContainer(tree);
   };
 
-  function renderContainer(current: ItemType[]) {
+  function renderContainer(current: ItemType[], root: number) {
     return (
       current &&
       current.map((item) =>
@@ -71,11 +86,11 @@ export default function Index() {
               animation={200}
               list={item.children}
               setList={(e) => {
-                console.log(90, item.children);
-                setMainContainer(e, !item.parentId, item);
+                console.log(root, e, item, item.children);
+                setMainContainer(e, false, item);
               }}
             >
-              {item.children && renderContainer(item.children)}
+              {(item.children && renderContainer(item.children, root + 1)) || null}
             </ReactSortable>
           </div>
         ) : (
@@ -127,10 +142,11 @@ export default function Index() {
           }}
           list={container}
           setList={(e) => {
+            console.log(333);
             setMainContainer(e, true);
           }}
         >
-          {renderContainer(container)}
+          {renderContainer(container, 0)}
         </ReactSortable>
       </div>
       <div className='form-sandbox__dispose'></div>
