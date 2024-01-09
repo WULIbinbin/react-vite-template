d5dbc4106d29245648e941e6150e466b4103a866
吴焯斌 <920658022@qq.com>
2024-01-09 17:50:14 +0800

    feat: eslint

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,69 +1,44 @@
-import { ELayoutStyle } from '@/types/layout.d'
-import { IRouter } from '@/types/router.d'
-import { lazy } from 'react'
+import { ELayoutStyle } from '@/types/layout.d';
+import { IRouter } from '@/types/router.d';
+import { lazy } from 'react';
 
-const Index = lazy(
-  () => import(/* webpackChunkName: "main" */ '@/views/index/index')
-)
-const Login = lazy(
-  () => import(/* webpackChunkName: "main" */ '@/views/account/login/index')
-)
-const Error = lazy(
-  () => import(/* webpackChunkName: "main" */ '@/views/error/index')
-)
-const InvoiceQuery = lazy(
-  () =>
-    import(
-      /* webpackChunkName: "invoice" */ '@/views/invoice/invoice-query/index'
-    )
-)
-const InvoiceCheck = lazy(
-  () =>
-    import(
-      /* webpackChunkName: "invoice" */ '@/views/invoice/invoice-check/index'
-    )
-)
-const InvoiceDetail = lazy(
-  () =>
-    import(
-      /* webpackChunkName: "invoice" */ '@/views/invoice/invoice-detail/index'
-    )
-)
-const FormSandbox = lazy(
-  () => import(/* webpackChunkName: "form" */ '@/views/form/sandbox/index')
-)
-const TipTap = lazy(
-  () => import(/* webpackChunkName: "form" */ '@/views/tiptap/index')
-)
+const Index = lazy(() => import(/* webpackChunkName: "main" */ '@/views/index/index'));
+const Login = lazy(() => import(/* webpackChunkName: "main" */ '@/views/account/login/index'));
+const Error = lazy(() => import(/* webpackChunkName: "main" */ '@/views/error/index'));
+const InvoiceQuery = lazy(() => import(/* webpackChunkName: "invoice" */ '@/views/invoice/invoice-query/index'));
+const InvoiceCheck = lazy(() => import(/* webpackChunkName: "invoice" */ '@/views/invoice/invoice-check/index'));
+const InvoiceDetail = lazy(() => import(/* webpackChunkName: "invoice" */ '@/views/invoice/invoice-detail/index'));
+const FormSandbox = lazy(() => import(/* webpackChunkName: "form" */ '@/views/form/sandbox/index'));
+const TipTap = lazy(() => import(/* webpackChunkName: "form" */ '@/views/tiptap/index'));
 
 export const routes: IRouter[] = [
   {
     path: '*',
-    redirect: '/index'
+    redirect: '/index',
   },
   {
     path: '/error/:type',
     meta: { title: '跳转错误', layoutStyle: ELayoutStyle.FullPage },
-    component: Error
+    component: Error,
   },
   {
     path: '/',
     index: true,
     // 默认重定向到首页
-    redirect: '/account/login'
+    redirect: '/account/login',
   },
   {
     path: '/index',
     meta: {
       title: '首页',
       showBreadcrumb: true,
-      layoutStyle: ELayoutStyle.Mix
+      layoutStyle: ELayoutStyle.Mix,
     },
-    component: Index
+    component: Index,
   },
   {
     path: '/account',
-    redirect: '/account/login'
+    redirect: '/account/login',
   },
   {
     path: '/account',
@@ -71,9 +46,9 @@ export const routes: IRouter[] = [
       {
         path: '/login',
         meta: { title: '用户登录', layoutStyle: ELayoutStyle.FullPage },
-        component: Login
-      }
-    ]
+        component: Login,
+      },
+    ],
   },
   {
     path: '/invoice',
@@ -84,33 +59,33 @@ export const routes: IRouter[] = [
         meta: {
           title: '发票查询',
           showBreadcrumb: true,
-          layoutStyle: ELayoutStyle.Mix
+          layoutStyle: ELayoutStyle.Mix,
         },
-        component: InvoiceQuery
+        component: InvoiceQuery,
       },
       {
         path: '/invoice-detail',
         meta: {
           title: '发票详情',
           showBreadcrumb: true,
-          layoutStyle: ELayoutStyle.FullPage
+          layoutStyle: ELayoutStyle.FullPage,
         },
-        component: InvoiceDetail
+        component: InvoiceDetail,
       },
       {
         path: '/invoice-check',
         meta: {
           title: '发票查验',
           showBreadcrumb: true,
-          layoutStyle: ELayoutStyle.Mix
+          layoutStyle: ELayoutStyle.Mix,
         },
-        component: InvoiceCheck
-      }
-    ]
+        component: InvoiceCheck,
+      },
+    ],
   },
   {
     path: '/invoice',
-    redirect: '/invoice/invoice-query'
+    redirect: '/invoice/invoice-query',
   },
   {
     path: '/form',
@@ -121,23 +96,23 @@ export const routes: IRouter[] = [
         meta: {
           title: '表单设计',
           showBreadcrumb: true,
-          layoutStyle: ELayoutStyle.Mix
+          layoutStyle: ELayoutStyle.Mix,
         },
-        component: FormSandbox
-      }
-    ]
+        component: FormSandbox,
+      },
+    ],
   },
   {
     path: '/form',
-    redirect: '/form/sandbox'
+    redirect: '/form/sandbox',
   },
   {
     path: '/tiptap',
     meta: {
       title: 'tiptap',
       showBreadcrumb: false,
-      layoutStyle: ELayoutStyle.FullPage
+      layoutStyle: ELayoutStyle.FullPage,
     },
-    component: TipTap
-  }
-]
+    component: TipTap,
+  },
+];
--- src/views/tiptap/index.tsx
+++ src/views/tiptap/index.tsx
@@ -1,24 +1,24 @@
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import { useEffect } from 'react'
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import { useEffect } from 'react';
 
 export default function Index() {
   const editor = useEditor({
     extensions: [StarterKit],
     content: '<p>Hello World!</p>',
     onUpdate({ editor }) {
-      const json = editor.getJSON()
-      console.log(json)
-    }
-  })
+      const json = editor.getJSON();
+      console.log(json);
+    },
+  });
 
   useEffect(() => {
-    console.log(editor)
-  }, [editor])
+    console.log(editor);
+  }, [editor]);
 
   return (
-    <div className="editor">
+    <div className='editor'>
       <EditorContent editor={editor} />
     </div>
-  )
+  );
 }
